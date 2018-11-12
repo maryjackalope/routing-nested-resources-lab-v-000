439ab63e3a8f0027a0a42ac5ef57e90f168883c4
Mary Jack <maryjackinthebox@gmail.com>
2018-11-12 00:15:13 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,6 +16,16 @@ class SongsController < ApplicationController
 
   def show
     @song = Song.find(params[:id])
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+        if @song.nil?
+          flash[:alert] = "Sorry, song not found"
+          redirect_to artist_songs_path(@artist)
+        end
+      else
+        @song = Song.find(params[:id])
+      end
   end
 
   def new
